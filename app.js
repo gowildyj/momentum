c24163d4f32b859ae538248a80b8c36796fb1353
Stella Chung <gowildyj@gmail.com>
2023-05-22 22:06:31 +0900

    evenetListner

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-const age = parseInt(prompt("How old are you?"));
+const title = document.querySelector(".hello");
+title.innerText = "Click Me";
 
-if (isNaN(age) || age < 0) {
-  console.log("Please write a real positive number");
-} else if (age < 18) {
-  console.log("You are too young.");
-} else if (age >= 18 && age <= 50) {
-  console.log("You can drink");
-} else if (age > 50 && age <= 80) {
-  console.log("You should exercise");
-} else if (age === 100) {
-  console.log("wow you are wise");
-} else if (age > 80) {
-  console.log("You can do whatever you want");
+function handleTitleClick() {
+  if (title.style.color == "blue") {
+    title.style.color = "black";
+  } else {
+    title.style.color = "blue";
+  }
 }
+function handleMouseEnter() {
+  title.innerText = "Mouse is here";
+}
+function handleMouseLeave() {
+  title.innerText = "Mouse is gone";
+}
+
+title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
