bcda62ccefb6cbc6affc1d2eb6e2a44a927ec670
Stella Chung <gowildyj@gmail.com>
2023-05-22 23:03:32 +0900

    이벤트 핸들링 with if

--- app.js
+++ app.js
@@ -2,36 +2,13 @@ const title = document.querySelector(".hello");
 title.innerText = "Click Me";
 
 function handleTitleClick() {
-  if (title.style.color == "blue") {
-    title.style.color = "black";
+  const currentColor = title.style.color;
+  let newColor;
+  if (currentColor === "blue") {
+    newColor = "tomato";
   } else {
-    title.style.color = "blue";
+    newColor = "blue";
   }
+  title.style.color = newColor;
 }
-function handleMouseEnter() {
-  title.innerText = "Mouse is here";
-}
-function handleMouseLeave() {
-  title.innerText = "Mouse is gone";
-}
-function handleWindowResize() {
-  document.body.style.backgroundColor = "tomato";
-}
-function handleWindowCopy() {
-  alert("Copied");
-}
-function handleWindowOffline() {
-  alert("SOS, No WIFI");
-}
-function handleWindowOnline() {
-  alert("There shall be WIFI");
-}
-
 title.addEventListener("click", handleTitleClick);
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
